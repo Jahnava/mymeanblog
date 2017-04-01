ee65783f657a07ceaec8ceded12bea9985f575a0
Justin Dennison <justin1dennison@gmail.com>
2017-03-31 20:47:30 -0400

    we updated the create method in post controller in response to the schema change of posts that require an author id for the post. Moreover, we removed an extraneous .then in the post.service as well as handled initialization of the post controller when we navigate to the edit page versus the create page

--- public/js/controllers/post.controller.js
+++ public/js/controllers/post.controller.js
@@ -2,9 +2,13 @@
   angular.module('mymeanblog')
          .controller('PostController', PostController);
 
-  PostController.$inject = ['$scope', 'PostService', '$routeParams'];
+  PostController.$inject = ['$scope',
+                            'PostService',
+                            '$routeParams',
+                            'UserService',
+                            '$location'];
 
-  function PostController($scope, PostService, $routeParams){
+  function PostController($scope, PostService, $routeParams, UserService, $location){
     $scope.create = create;
     $scope.edit = edit;
     $scope.mockPost = {
@@ -14,21 +18,36 @@
       created: new Date(),
       updated: new Date()
     };
-    var id = $routeParams.postId;
-    PostService.getOne(id)
-                .then(function(){
-                  console.log('successs');
-                })
-                .catch(function(){
-                  console.log('errror');
-                })
 
+
+    if($location.path().startsWith('/edit/')){
+      editInit();
+    }
+    function editInit(){
+      var id = $routeParams.postId;
+      PostService.getOne(id)
+                  .then(function(){
+                    console.log('successs');
+                  })
+                  .catch(function(){
+                    console.log('errror');
+                  });
+    }
     function edit(post){
       console.log('editing the post');
     }
 
     function create(post){
-      PostService.create(post);
+      var userId = UserService.currentUser()._id;
+      console.log(userId);
+      post.author = userId;
+      PostService.create(post)
+                 .then(function(){
+                   $location.path('/dashboard');
+                 })
+                 .catch(function(err){
+                   console.log(err);
+                 });
     }
   }
 }());
